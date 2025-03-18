9fe86cae0381c68c619787ede3179a78e0e2160d
Kirieshka <kirill.danilov2001@mail.ru>
2025-03-18 19:01:54 +0300

    new-slider

--- js/main.js
+++ js/main.js
@@ -2,21 +2,10 @@ let swiper = new Swiper(".topSwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (index + 1) + "</span>";
-      },
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    autoplay: {
-        delay: 10000,
-        disableOnInteraction: false,
-    },
+    // autoplay: {
+    //     delay: 10000,
+    //     disableOnInteraction: false,
+    // },
 });
 
 let arr = [1962, 1981, 1990, 1992]
